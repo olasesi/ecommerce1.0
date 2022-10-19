f206d05b2ffef8e5a05d2e0d359f5b2e32fd7e64
unknown <ola.ses@yahoo.com>
2022-10-19 16:20:30 +0100

    first commit

---
+++ resources/js/components/Example.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import "../../css/app.css";
+
+function Example() {
+    return (
+        
+        <>
+            
+        </>
+       
+    );
+}
+
+export default Example;
+
+if (document.getElementById('example')) {
+    ReactDOM.render(<Example />, document.getElementById('example'));
+}
